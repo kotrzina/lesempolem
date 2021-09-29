f254e2b15df80ab0f1ac41aa847ae0b4cbd2d80d
Tomas Kozak <kozak@talko.cz>
2021-09-29 23:12:17 +0200

    feat: add google verification DNS record

--- terraform/dns.tf
+++ terraform/dns.tf
@@ -6,7 +6,7 @@ resource "cloudflare_record" "cname_lesempolem_cz" {
   zone_id = cloudflare_zone.lesempolem_cz.id
   name    = "lesempolem.cz"
   type    = "CNAME"
-  value = "lesempolem.pages.dev"
+  value   = "lesempolem.pages.dev"
   proxied = true
 }
 
@@ -14,24 +14,31 @@ resource "cloudflare_record" "cname_www_lesempolem_cz" {
   zone_id = cloudflare_zone.lesempolem_cz.id
   name    = "www"
   type    = "CNAME"
-  value = "lesempolem.pages.dev"
+  value   = "lesempolem.pages.dev"
   proxied = true
 }
 
 resource "cloudflare_record" "mx_1" {
-  zone_id = cloudflare_zone.lesempolem_cz.id
-  name    = "lesempolem.cz"
-  type    = "MX"
-  value = "d38c0c2a35c0e449.mx2.emailprofi.seznam.cz"
+  zone_id  = cloudflare_zone.lesempolem_cz.id
+  name     = "lesempolem.cz"
+  type     = "MX"
+  value    = "d38c0c2a35c0e449.mx2.emailprofi.seznam.cz"
   priority = 10
-  proxied = false
+  proxied  = false
 }
 
 resource "cloudflare_record" "mx_2" {
+  zone_id  = cloudflare_zone.lesempolem_cz.id
+  name     = "lesempolem.cz"
+  type     = "MX"
+  value    = "d38c0c2a35c0e449.mx1.emailprofi.seznam.cz"
+  priority = 20
+  proxied  = false
+}
+
+resource "cloudflare_record" "google_verification" {
   zone_id = cloudflare_zone.lesempolem_cz.id
   name    = "lesempolem.cz"
-  type    = "MX"
-  value = "d38c0c2a35c0e449.mx1.emailprofi.seznam.cz"
-  priority = 20
-  proxied = false
+  type    = "TXT"
+  value   = "google-site-verification=ZI4A65BCTQPahqvG4tUljiJEgagRmdjnQ_6mfxGvbN8"
 }
