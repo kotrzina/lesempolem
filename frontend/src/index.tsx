fdb063fc731d2eed3fb705f15b914515f2ff5459
Tomas Kozak <kozak@talko.cz>
2022-04-09 22:00:04 +0200

    Fix react 18 root element

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,7 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import {createRoot} from "react-dom/client";
+import React from 'react';
+
 
 if (window.location.host.startsWith("www.")) {
     const parts = window.location.host.split('.').slice(1)
@@ -9,10 +10,12 @@ if (window.location.host.startsWith("www.")) {
     window.location = window.location.protocol + "//" + parts.join('.') + window.location.pathname
 }
 
-ReactDOM.render(
-    <React.StrictMode>
-        <App/>
-    </React.StrictMode>,
-    document.getElementById('root')
+
+const root = createRoot(document.getElementById("root") as HTMLElement);
+root.render(
+    /* https://github.com/remix-run/react-router/issues/7870 */
+    /* <React.StrictMode> */
+    <App/>
+    /* </React.StrictMode> */
 );
 
