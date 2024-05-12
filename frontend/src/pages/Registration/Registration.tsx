0a693e738317dce5a12e7aadee1f20498d4f0a1f
Tomas Kozak <kozak@talko.cz>
2024-05-12 18:58:31 +0200

    Disable Borak registration

--- frontend/src/pages/Registration/Registration.tsx
+++ frontend/src/pages/Registration/Registration.tsx
@@ -4,9 +4,7 @@ import {RegistrationForm} from "./RegistrationForm/RegistrationForm";
 import {Registered} from "./Registered/Registered";
 import {fetchRegisteredRacers, Racer} from "../../api/backend";
 import './Registration.css'
-import {Button} from "react-bootstrap";
 import {LpDate} from "../../components/Date/LpDate";
-import address from "../../Address";
 
 interface Props {
     enabled: boolean
@@ -34,29 +32,12 @@ export const Registration: FC<Props> = (props) => {
         <>
             <h1>Registrace</h1>
 
-
             <p hidden={true}>
                 Předběžná registrace na Lesempolem, které se uskuteční <LpDate type={"lp"} format={"short"}/>
                 , je k dispozici na stránkách OBL.
                 Pokud stále nejsi rozhodnutý, rozklikni si další pravidla a informace o Lesempolem.
             </p>
 
-            <h2>Borák - Lesepolem za tvarůžkem</h2>
-            <p>
-                <p>
-                    Ultramaraton Borák - Lesempolem - závod Ultracupu 2024, který proběhne na trati Lesempolem
-                    11.&nbsp;5.&nbsp;2024
-                </p>
-                <Button
-                    href={address.borak}
-                    target={"_blank"}
-                    variant={"success"}
-                >
-                    Registrace na Borák
-                </Button>
-            </p>
-
-            <h2>Lesempolem</h2>
             <p>
                 Tento rok už Lesempolem proběhlo. Díky a uvidíme se příští rok!
             </p>
