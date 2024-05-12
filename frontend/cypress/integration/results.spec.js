d4ddc76ca872057f35e92c04744477722932c50d
Tomas Kozak <kozak@talko.cz>
2024-05-12 19:05:59 +0200

    Fix tests

--- frontend/cypress/integration/results.spec.js
+++ frontend/cypress/integration/results.spec.js
@@ -2,7 +2,7 @@ function testResults(year) {
     cy.visit("/vysledky.html")
     cy.contains(year).click()
 
-    cy.contains("Výsledky závodu v roce " + year)
+    cy.contains("Výsledky závodu")
     cy.contains("Poř.")
     cy.contains("S. číslo")
     cy.contains("Jméno")
@@ -12,17 +12,18 @@ function testResults(year) {
 describe('results tests', () => {
     it("test all result pages", () => {
         const years = [
-            2024,
-            2023,
-            2022,
-            2021,
-            2019,
-            2018,
-            2017,
-            2016,
-            2015,
-            2014,
-            2013,
+            "Borák 2024",
+            "2024",
+            "2023",
+            "2022",
+            "2021",
+            "2019",
+            "2018",
+            "2017",
+            "2016",
+            "2015",
+            "2014",
+            "2013",
         ]
 
         years.forEach(year => {
